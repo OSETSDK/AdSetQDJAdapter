6927e7aa8636f4273db151360836ef7ae47c25af
yaohaofei <313888178@qq.com>
2026-09-22 09:30:02 +0800

    feat: release v2.0.0.0

---
+++ AdSetQDJAdapter.xcframework/ios-arm64_x86_64-simulator/AdSetQDJAdapter.framework/Headers/AdSetQDJAdapter.h
@@ -0,0 +1,10 @@
+//
+//  AdSetQDJAdapter.h
+//  AdSetQDJAdapter
+//  AdSetQDJ（掌上）渠道适配器
+//
+
+#import <Foundation/Foundation.h>
+
+FOUNDATION_EXPORT double AdSetQDJAdapterVersionNumber;
+FOUNDATION_EXPORT const unsigned char AdSetQDJAdapterVersionString[];
